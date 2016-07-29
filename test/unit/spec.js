ea4899db022d9b02149bb74e0b418f32fc1a9e7c
rodoabad <rodoabad@gmail.com>
2016-07-29 18:44:06 -0500

    chore: show import problem with issue 44

--- test/unit/spec.js
+++ test/unit/spec.js
@@ -1,7 +1,16 @@
+import {rawr} from '../../src/test';
+import {expect} from 'code';
+
 describe('suite', () => {
-  it('case', (done) => {
-    setTimeout(() => {
-      done();
-    }, 4000);
-  });
+    it('case', (done) => {
+        setTimeout(() => {
+            done();
+        }, 4000);
+    });
+
+    it('case2', () => {
+
+        expect(rawr()).equal(2);
+
+    })
 });
